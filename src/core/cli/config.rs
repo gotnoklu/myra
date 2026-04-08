fb5f76c6923474a776fd02e0e6826a2025edf296
Enoch Klu <eenoch@delalify.com>
2026-04-08 15:01:14 +0000

    fix: pass matched args directly to CLI arg matcher functions

--- src/core/cli/config.rs
+++ src/core/cli/config.rs
@@ -11,9 +11,9 @@ pub fn register_cli_args() -> ArgMatches {
 }
 
 pub fn match_cli_args(matches: &ArgMatches) {
-    if matches.subcommand_matches("templates").is_some() {
-        match_template_cli_args(&matches)
-    } else if matches.subcommand_matches("projects").is_some() {
-        match_project_cli_args(&matches)
+    if let Some(matched) = matches.subcommand_matches("templates") {
+        match_template_cli_args(&matched)
+    } else if let Some(matched) = matches.subcommand_matches("projects") {
+        match_project_cli_args(&matched)
     }
 }
