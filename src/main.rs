7f26767592e42cf1f008d7bf01559ca9f807dfb3
Enoch Klu <eenoch@delalify.com>
2026-04-08 14:48:03 +0000

    chore: update function import and usage

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod core;
 mod modules;
 
-use crate::core::cli::config::{parse_cli_args, register_cli_args};
+use crate::core::cli::config::{match_cli_args, register_cli_args};
 
 fn main() {
     let arg_matches = register_cli_args();
 
-    parse_cli_args(&arg_matches);
+    match_cli_args(&arg_matches);
 }
